328aa702f62969f1ec1922dd0d4c88d083f68e05
zaylour <604602435@qq.com>
2019-12-05 18:53:18 +0800

    2019-12-5 第一次提交

--- app/src/main/java/com/zay/FlexibleTag/CustomView/TagActivity.kt
+++ app/src/main/java/com/zay/FlexibleTag/CustomView/TagActivity.kt
@@ -1,6 +1,8 @@
 package com.zay.FlexibleTag.CustomView
 
+import android.animation.ValueAnimator
 import android.os.Bundle
+import android.view.View
 import android.view.animation.AnimationUtils
 import android.view.animation.RotateAnimation
 import androidx.appcompat.app.AppCompatActivity
@@ -65,5 +67,42 @@ class TagActivity :AppCompatActivity() {
             view!!.clearAnimation()
         }
     }
-
+    //整体移动--用属性动画
+    fun viewMove(view : View){
+        val moveAnin=ValueAnimator.ofInt(0,3)
+        // ofInt（）作用有两个
+        // 1. 创建动画实例
+        // 2. 将传入的多个Int参数进行平滑过渡:此处传入0和1,表示将值从0平滑过渡到1
+        // 如果传入了3个Int参数 a,b,c ,则是先从a平滑过渡到b,再从b平滑过渡到C，以此类推
+        // ValueAnimator.ofInt()内置了整型估值器,直接采用默认的.不需要设置，即默认设置了如何从初始值 过渡到 结束值
+        // 步骤2：设置动画的播放各种属性
+        moveAnin.setDuration(500);
+        // 设置动画运行的时长
+        moveAnin.setStartDelay(500);
+        // 设置动画延迟播放时间
+        moveAnin.setRepeatCount(0);
+        // 设置动画重复播放次数 = 重放次数+1
+        // 动画播放次数 = infinite时,动画无限重复
+        moveAnin.setRepeatMode(ValueAnimator.RESTART);
+        // 设置重复播放动画模式
+        // ValueAnimator.RESTART(默认):正序重放
+        // ValueAnimator.REVERSE:倒序回放
+        // 步骤3：将改变的值手动赋值给对象的属性值：通过动画的更新监听器
+        // 设置 值的更新监听器
+        // 即：值每次改变、变化一次,该方法就会被调用一次
+        moveAnin.addUpdateListener(object :ValueAnimator.AnimatorUpdateListener {
+            override fun onAnimationUpdate(animation: ValueAnimator?) {
+                var currentValue:Int = animation!!.getAnimatedValue() as Int;
+                // 获得改变后的值
+                System.out.println(currentValue);
+                // 输出改变后的值
+                // 步骤4：将改变后的值赋给对象的属性值，下面会详细说明
+                //view.height=currentValue
+                // 步骤5：刷新视图，即重新绘制，从而实现动画效果
+                view.requestLayout();
+            }
+        });
+        moveAnin.start();
+        // 启动动画
+    }
 }
